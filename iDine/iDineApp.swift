6beb2ed226e4c022afc2f30c44a4b912cbf1102f
sreinin <bosreynin7@gmail.com>
2023-06-27 14:26:26 +0900

    my commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Sreynin on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
